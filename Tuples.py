0baa9752be7088fe123c013cd009502be2e9abf8
Hamdi Khalil <38282254+hamdi3@users.noreply.github.com>
2021-01-07 16:23:05 +0100

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+#Tupels
+t= (1,2,3,3)
+print(t.count(3)) # this shows us how many times the 3 was repeated in t
+print(t.index(2)) #this gives us the indx of a specific element
+#tupels dont have any other methods, they are unchangable
+t=((1,2),"hello",[1,2,3],4)
+print(t[0])
