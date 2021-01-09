3211538d26f01ebc7ae93f8ce801902b914b7923
Hamdi Khalil <38282254+hamdi3@users.noreply.github.com>
2021-01-09 15:00:08 +0100

    Create ImportsAndModels.py

---
+++ ImportsAndModels.py
@@ -0,0 +1,8 @@
+#moudle , Pakete und importe
+#dealing with moudle
+import math #doing this we got the whole math lib
+print(math.ceil(2.4))
+from math import ceil #doing this we just imported ceil 
+print(ceil(2.7)) #since we only imported ceil no need to refrence math
+print(dir(math)) # to know all funcs in math
+print(help(ceil)) # to know exactly what a func does
