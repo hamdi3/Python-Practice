f76259f63520dd55f16ae8bbb1d93cd9aee8b7ae
Hamdi Khalil <38282254+hamdi3@users.noreply.github.com>
2021-01-09 14:57:56 +0100

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -84,3 +84,37 @@ print(len(buch))
 print(str(buch))
 del buch
 print(buch) #we realize that this prints an error since we already deleted the data
+
+#OOP Assessment
+class Linie(object):
+    def __init__(self,koor1,koor2):
+        print("eine Linie wurde erstellt")
+        self.koor1 = koor1
+        self.koor2 = koor2
+    def distanz (self):
+        x1, y1 = self.koor1
+        x2, y2 = self.koor2
+        return ((x2-x1)**2 + (y2-y1)**2)**0.5
+    def steigung(self):
+        return ((self.koor2[1]-self.koor1[1])/(self.koor2[0]-self.koor1[0])) #or just deal with them as if they were a list 
+
+koordinate1 = (3,2)
+koordinate2 = (8,10)
+li = Linie(koordinate1,koordinate2)
+print(li.distanz())
+print(li.steigung())
+
+import math
+class Zylinder (object):
+    def __init__(self, hohe = 1, radius = 1):
+        print("eine Zylinder wurde erstellt")
+        self.hohe = hohe
+        self.radius = radius
+    def vol (self):
+        return math.pi * self.radius**2 * self.hohe
+    def oberflaeche (self):
+        return 2 * math.pi * self.radius * (self.radius + self.hohe)
+
+c = Zylinder(2,3)
+print(c.vol())
+print(c.oberflaeche())
