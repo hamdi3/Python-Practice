5a9265b866bc4fb988c925110892726dd5842d01
Hamdi Khalil <38282254+hamdi3@users.noreply.github.com>
2021-01-09 14:51:51 +0100

    Create Methods.py

---
+++ Methods.py
@@ -0,0 +1,5 @@
+#Mehods in python all have the form of "object.method(parameter)" where object could refer to a list or a string...Etc
+l = [1,2,3]
+l.append(6) # this one is a method object.method (parameter)
+l.count(2)
+help(l.count) #this one is used to know what a methods exactly does
